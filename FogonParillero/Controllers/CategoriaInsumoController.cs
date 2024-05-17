af7d4125f32e6ff2591d904f51ce761fae5a9564
Luis <N00294984@upn.pe>
2024-05-16 23:12:23 -0500

    haciendo cat insumos

--- FogonParillero/Controllers/CategoriaInsumoController.cs
+++ FogonParillero/Controllers/CategoriaInsumoController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FogonParillero.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FogonParillero.Controllers
 {
+
     public class CategoriaInsumoController : Controller
     {
-        public IActionResult Index()
+        private readonly ICategoriaInsumoInterface _categoria;
+
+        public CategoriaInsumoController(ICategoriaInsumoInterface categoria)
         {
-            return View();
+            _categoria = categoria;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var categorias = await _categoria.ObtenerTodosAsync();
+            ViewData["ModalAbierto"] = false;
+            return View(categorias);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Registrar()
+        {
+            ViewData["ModalAbierto"] = true;
+            var categorias = await _categoria.ObtenerTodosAsync();
+            return View("Index", categorias);
+        }
+
+        [HttpPost]
+        public IActionResult CerrarModal()
+        {
+            ViewData["ModalAbierto"] = false;
+            // Redireccionar a la ruta /categoriaproducto
+            return RedirectToAction("Index", "CategoriaProducto");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GuardarNuevaCategoria(string nombreCategoria)
+        {
+            await _categoria.Registrar(nombreCategoria);
+            return RedirectToAction("Index");
+        }
+
+
+
+
+
+
+
+
     }
 }
