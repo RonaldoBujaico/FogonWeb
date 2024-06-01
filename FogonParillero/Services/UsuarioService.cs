97f89f3339642b36b18c358d50dc168d9d2fb712
Andres S <n00245955@upn.pe>
2024-06-01 12:56:23 -0500

    Implementación de SHA512 para la contraseña del usuario

--- FogonParillero/Services/UsuarioService.cs
+++ FogonParillero/Services/UsuarioService.cs
@@ -1,6 +1,8 @@
 ﻿using FogonParillero.Data;
 using FogonParillero.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace FogonParillero.Services
 {
@@ -15,8 +17,26 @@ namespace FogonParillero.Services
 
         public async Task<bool> AutenticarUsuarioAsync(string dni, string contraseña)
         {
-            var usuario = await _contexto.Usuarios.FirstOrDefaultAsync(u => u.Dni == dni && u.Contraseña == contraseña);
+            string hashedContraseña = GenerarHashSHA512(contraseña);
+            var usuario = await _contexto.Usuarios.FirstOrDefaultAsync(u => u.Dni == dni && u.Contraseña == hashedContraseña);
             return usuario != null;
+        }
+
+        private string GenerarHashSHA512(string input)
+        {
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+                byte[] hashBytes = sha512.ComputeHash(inputBytes);
+
+                StringBuilder stringBuilder = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    stringBuilder.Append(b.ToString("X2")); 
+                }
+                return stringBuilder.ToString();
+            }
         }
     }
 }
