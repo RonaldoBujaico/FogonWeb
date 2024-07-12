9e9b8ba997c1d148a66b36f13be2f0d227df1513
Andres S <n00245955@upn.pe>
2024-07-11 22:29:07 -0500

    Fix

--- FogonParillero/Data/ApplicationContext.cs
+++ FogonParillero/Data/ApplicationContext.cs
@@ -14,15 +14,10 @@ namespace FogonParillero.Data
         public DbSet<Producto> Productos { get; set; }
         public DbSet<CategoriaInsumo> CategoriasInsumo { get; set; }
         public DbSet<Insumo> Insumos { get; set; }
-<<<<<<< HEAD
         public DbSet<Mesa> Mesa { get; set; }
         public DbSet<Unidad> Unidades { get; set; }
         public DbSet<Pedido> Pedido { get; set; }
         public DbSet<DetalleInsumo> DetalleInsumos { get; set; }
         public DbSet<PedidoDetalle> PedidoDetalle { get; set; }
-=======
-        public DbSet<Unidad> Unidades { get; set; }
-        public DbSet<DetalleInsumo> DetalleInsumos { get; set; }
->>>>>>> 2045db2c0ccac8e406a72b96d93ff057c405aa53
     }
 }
